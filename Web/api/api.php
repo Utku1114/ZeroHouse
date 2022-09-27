23d4a9471d3c0d2904685d1b8f8c83594c08835a
Blackjack <78551687+Utku1114@users.noreply.github.com>
2022-09-27 17:50:51 +0300

    Update api.php
    WAF Eklendi

--- Web/api/api.php
+++ Web/api/api.php
@@ -2,6 +2,8 @@
 
 require_once "config.php";
 
+/* Block durumunda siliniz. */require('WAF.php');	$xWAF = new xWAF(); 	$xWAF->useCloudflare(); 	$xWAF->useBlazingfast(); 	$xWAF->customIPHeader('IP-Header'); 	$xWAF->antiCookieSteal('username'); 	$xWAF->checkGET();	$xWAF->checkPOST(); $xWAF->checkCOOKIE();
+
 $gelentur = $_GET['tur'];
 $gelenanahtar = $_GET['anahtar'];
 $gelenveri = $_GET['veri'];
