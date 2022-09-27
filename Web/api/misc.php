eceac94063c0c63d5f503486f4bc1cc18b00bfb0
Blackjack <78551687+Utku1114@users.noreply.github.com>
2022-09-27 17:53:14 +0300

    Update misc.php
    WAF Eklendi.

--- Web/api/misc.php
+++ Web/api/misc.php
@@ -1,5 +1,22 @@
 <?php
 require_once "config.php";
+/* Block durumunda siliniz. */
+        require('WAF.php');
+	$xWAF = new xWAF();
+
+	$xWAF->useCloudflare();
+
+	$xWAF->useBlazingfast();
+
+	$xWAF->customIPHeader('IP-Header');
+
+	$xWAF->antiCookieSteal('username'); 
+
+
+	$xWAF->checkGET();
+	$xWAF->checkPOST();
+        $xWAF->checkCOOKIE();
+
 								$sql=$pdo->prepare("select * from roleler");
 $sql->execute();
 while($row=$sql->fetch(PDO::FETCH_ASSOC)){
